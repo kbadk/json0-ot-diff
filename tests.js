d115372e179be86404e05caf6eba0318b265b26c
Curran <curran.kelleher@gmail.com>
2021-11-28 06:38:04 -0500

    Fix tabs vs spaces.

--- tests.js
+++ tests.js
@@ -87,42 +87,42 @@ var tests = [
 		[	{} ]
 	],
 	// string tests
-  // Inspired by https://github.com/google/diff-match-patch/blob/master/javascript/tests/diff_match_patch_test.js
-  ["abc", "xyz"],
-  ["1234abcdef", "1234xyz"],
-  ["1234", "1234xyz"],
-  ["abc", "xyz"],
-  ["abcdef1234", "xyz1234"],
-  ["1234", "xyz1234"],
-  ["", "abcd"],
-  ["abc", "abcd"],
-  ["123456", "abcd"],
-  ["123456xxx", "xxxabcd"],
-  ["fi", "\ufb01i"],
-  ["1234567890", "abcdef"],
-  ["12345", "23"],
-  ["1234567890", "a345678z"],
-  ["a345678z", "1234567890"],
-  ["abc56789z", "1234567890"],
-  ["a23456xyz", "1234567890"],
-  ["121231234123451234123121", "a1234123451234z"],
-  ["x-=-=-=-=-=-=-=-=-=-=-=-=", "xx-=-=-=-=-=-=-="],
-  ["-=-=-=-=-=-=-=-=-=-=-=-=y", "-=-=-=-=-=-=-=yy"],
-  ["qHilloHelloHew", "xHelloHeHulloy"],
-  ["abcdefghijk", "fgh"],
-  ["abcdefghijk", "efxhi"],
-  ["abcdefghijk", "cdefxyhijk"],
-  ["abcdefghijk", "bxy"],
-  ["123456789xx0", "3456789x0"],
-  ["abcdefghijk", "efxyhi"],
-  ["abcdefghijk", "bcdef"],
-  ["abcdexyzabcde", "abccde"],
-  ["abcdefghijklmnopqrstuvwxyz01234567890", "XabXcdXefXghXijXklXmnXopXqrXstXuvXwxXyzX01X23X45X67X89X0"],
-  ["abcdef1234567890123456789012345678901234567890123456789012345678901234567890uvwxyz", "abcdefuvwxyz"],
-  ["1234567890123456789012345678901234567890123456789012345678901234567890", "abc"],
-  ["XY", "XtestY"],
-  ["XXXXYYYY", "XXXXtestYYYY"],
-  ["The quick brown fox jumps over the lazy dog.", "Woof"],
+	// Inspired by https://github.com/google/diff-match-patch/blob/master/javascript/tests/diff_match_patch_test.js
+	["abc", "xyz"],
+	["1234abcdef", "1234xyz"],
+	["1234", "1234xyz"],
+	["abc", "xyz"],
+	["abcdef1234", "xyz1234"],
+	["1234", "xyz1234"],
+	["", "abcd"],
+	["abc", "abcd"],
+	["123456", "abcd"],
+	["123456xxx", "xxxabcd"],
+	["fi", "\ufb01i"],
+	["1234567890", "abcdef"],
+	["12345", "23"],
+	["1234567890", "a345678z"],
+	["a345678z", "1234567890"],
+	["abc56789z", "1234567890"],
+	["a23456xyz", "1234567890"],
+	["121231234123451234123121", "a1234123451234z"],
+	["x-=-=-=-=-=-=-=-=-=-=-=-=", "xx-=-=-=-=-=-=-="],
+	["-=-=-=-=-=-=-=-=-=-=-=-=y", "-=-=-=-=-=-=-=yy"],
+	["qHilloHelloHew", "xHelloHeHulloy"],
+	["abcdefghijk", "fgh"],
+	["abcdefghijk", "efxhi"],
+	["abcdefghijk", "cdefxyhijk"],
+	["abcdefghijk", "bxy"],
+	["123456789xx0", "3456789x0"],
+	["abcdefghijk", "efxyhi"],
+	["abcdefghijk", "bcdef"],
+	["abcdexyzabcde", "abccde"],
+	["abcdefghijklmnopqrstuvwxyz01234567890", "XabXcdXefXghXijXklXmnXopXqrXstXuvXwxXyzX01X23X45X67X89X0"],
+	["abcdef1234567890123456789012345678901234567890123456789012345678901234567890uvwxyz", "abcdefuvwxyz"],
+	["1234567890123456789012345678901234567890123456789012345678901234567890", "abc"],
+	["XY", "XtestY"],
+	["XXXXYYYY", "XXXXtestYYYY"],
+	["The quick brown fox jumps over the lazy dog.", "Woof"],
 	// big tests
 	[
 		[],
@@ -199,9 +199,9 @@ tests.forEach(function([input, output]) {
 tests.forEach(function([input, output]) {
 	var ops = jsondiff(input, output);
 
-  // Don't let json0 mutate the input,
-  // so we can use it in later tests.
-  input = clone(input);
+	// Don't let json0 mutate the input,
+	// so we can use it in later tests.
+	input = clone(input);
 	ops.forEach(function(op) {
 		assert.doesNotThrow(function() {
 			input = json0.type.apply(input, [op]);
